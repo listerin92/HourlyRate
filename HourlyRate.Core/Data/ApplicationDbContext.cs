31ef385ea4b5fbf4fdb6f1757159671dc79cb72f
Dobromir Petrov <luzterin@gmail.com>
2022-10-20 22:10:39 +0300

    initial git push, core project sucessful

---
+++ HourlyRate.Core/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace HourlyRate.Core.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- HourlyRate/Program.cs
+++ HourlyRate/Program.cs
@@ -1,4 +1,4 @@
-using HourlyRate.Data;
+using HourlyRate.Core.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
